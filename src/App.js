73bf9aeb01f4abc90d02357ddd5f1785d68b6df9
Jinwoo Kawk <tkfkdnwjd@gmail.com>
2020-10-20 17:50:40 +0900

    class 2.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Potato(){
-  return <h3>I like potato</h3>;
+const foods = [
+  { id: 1, name: 'pizza', image: 'https://picsum.photos/id/1/200', rating: 5 },
+  {
+    id: 2,
+    name: 'kimchi',
+    image: 'https://picsum.photos/id/2/200',
+    rating: 4.3,
+  },
+  { id: 3, name: 'rice', image: 'https://picsum.photos/id/3/200', rating: 4.9 },
+  {
+    id: 4,
+    name: 'kimbap',
+    image: 'https://picsum.photos/id/4/200',
+    rating: 4.2,
+  },
+];
+
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Potato />
+      {foods.map((dish) => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
